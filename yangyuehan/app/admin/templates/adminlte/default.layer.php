15cb2e96f4a207e61b5c249b478e470649dcf57d
yangzhiweiga@163.com <285978725ga>
2018-05-09 16:15:12 +0800

    导航

--- yangyuehan/app/admin/templates/adminlte/default.layer.php
+++ yangyuehan/app/admin/templates/adminlte/default.layer.php
@@ -42,9 +42,9 @@
         <nav class="navbar navbar-static-top" role="navigation">
             <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
                 <span class="sr-only">Toggle navigation</span>
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
             </a>
             <div class="navbar-custom-menu">
                 <ul class="nav navbar-nav">
